700ca6b6cc428106a98659802889c52d4bd49143
DelitaBrTinambunan16 <delita24simahasiswa.pcr.ac.id>
2025-10-23 10:48:11 +0700

    comit user p7

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\UserController;
 use App\Http\Controllers\QuestionController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\MahasiswaController;
@@ -45,4 +46,4 @@ Route::get('/about', function () {
 
 Route::resource('pelanggan', PelangganController::class);
 
-
+Route::resource('user', UserController::class);
